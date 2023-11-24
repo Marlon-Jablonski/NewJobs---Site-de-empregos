b6e39d475d5d153a9eb64aa00d8976e440720afe
PedroBarrionuevoChagasdosSantos <pedrinhobcs@gmail.com>
2023-11-24 20:53:43 -0300

    Update (backend)

--- Backend/Backend (NewJOBS) ATUALIZADO 22-11/src/main/java/testebackend/testebackend/Controller/Login/LoginController.java
+++ Backend/Backend (NewJOBS) ATUALIZADO 22-11/src/main/java/testebackend/testebackend/Controller/Login/LoginController.java
@@ -1,4 +1,52 @@
 package testebackend.testebackend.Controller.Login;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import testebackend.testebackend.Model.Login.LoginDAO;
+import testebackend.testebackend.Model.Login.LoginEntity;
+import testebackend.testebackend.Model.Usuario.UsuarioDAO;
+import java.util.UUID;
 
+@RestController
+@RequestMapping("/login/")
 public class LoginController {
+
+    @Autowired
+    public UsuarioDAO usuarioDAO;
+
+    @Autowired
+    public LoginDAO loginDAO;
+
+    @PostMapping()
+    @CrossOrigin(origins = "*")
+    public ResponseEntity<String> postLogin(@RequestBody LoginDTO dto) {
+
+        // Verificar se o usuário existe
+        int id = usuarioDAO.getByUsuario(dto.usuario, dto.senha);
+
+        if (id == -1) {
+
+            // Retornar 401
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+        } else {
+
+            // Gravar o token no banco
+            String token = UUID.randomUUID().toString();
+            LoginEntity entity = new LoginEntity();
+            entity.token = token;
+            entity.usuario_id = id;
+
+
+            loginDAO.add(entity);
+
+
+            // Retornar o token
+            return ResponseEntity.ok().body(token);
+
+        }
+    }
+
+
 }
--- Backend/Backend (NewJOBS) ATUALIZADO 22-11/src/main/java/testebackend/testebackend/Model/Usuario/UsuarioDAO.java
+++ Backend/Backend (NewJOBS) ATUALIZADO 22-11/src/main/java/testebackend/testebackend/Model/Usuario/UsuarioDAO.java
@@ -1,4 +1,33 @@
 package testebackend.testebackend.Model.Usuario;
 
+import testebackend.testebackend.ConnectionSingleton;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class UsuarioDAO {
+
+    public int getByUsuario(String usuario, String senha) {
+        final String sql = "SELECT usuario_id FROM usuario WHERE usuario = ? and senha = ?";
+        try (final PreparedStatement preparedStatement = ConnectionSingleton.getConnection().prepareStatement(sql)) {
+            preparedStatement.setString(1, usuario);
+            preparedStatement.setString(2, senha);
+
+            try (final ResultSet resultadoPessoas = preparedStatement.executeQuery()) {
+                if (!resultadoPessoas.next()) {
+                    return -1 ;
+                }
+
+                int id = resultadoPessoas.getInt("usuario_id");
+
+
+                return id;
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
