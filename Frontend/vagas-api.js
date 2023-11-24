b40838fab21307db558840ca07ee4c8de5eb9d45
PedroBarrionuevoChagasdosSantos <pedrinhobcs@gmail.com>
2023-11-24 18:38:44 -0300

    Get Vagas (24/11)

--- Frontend/vagas-api.js
+++ Frontend/vagas-api.js
@@ -1,5 +1,5 @@
 async function getVagas() {
-    let response = await fetch("https://6510cd873ce5d181df5d81c0.mockapi.io/api/vagas/vagas");
+    let response = await fetch("http:/localhost:8080/vagas");
     let vagas = await response.json();
 
     return vagas;
@@ -11,7 +11,7 @@ getVagas().then(vagas => vagas.forEach(vaga => {
     let coluna = document.createElement("div");
     //  adiciona a classe "col-4" ao elemento da coluna e "mb-3" para dar uma margem entre as linhas.
     coluna.classList.add("col-4", "mb-5");
-    coluna.innerHTML = "<div class='card' style='width: 18rem;'><div class='card-body'><h5 class='card-title'>" + vaga.vaga + "</h5>  <p class='card-text'> " + vaga.descricao + " </p> </div><ul class='list-group list-group-flush'><li class='list-group-item'>" + vaga.cidade + "</li><li class='list-group-item'>" + vaga.empresa + "</li></ul><a href='#' class='btn btn-primary'>Candidatar se</a></div>";
+    coluna.innerHTML = "<div class='card' style='width: 18rem;'><div class='card-body'><h5 class='card-title'>" + vaga.nomedavaga + "</h5>  <p class='card-text'> " + vaga.descricaodavaga + " </p> </div><ul class='list-group list-group-flush'><li class='list-group-item'>" + vaga.nomedacidade + "</li><li class='list-group-item'>" + vaga.nomedaempresa + "</li></ul><a href='#' class='btn btn-primary'>Candidatar se</a></div>";
 
     linha.appendChild(coluna);
     //adiciona uma coluna rescem cria a linha.
