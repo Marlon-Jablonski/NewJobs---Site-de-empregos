ae8c21f44539947b86bf6d52fc1ed422dd7b65a6
Marlon-Jablonski <marlonjablonski16@gmail.com>
2023-11-24 20:53:31 -0300

    Alteração Frontend

--- Frontend/vagas-api.js
+++ Frontend/vagas-api.js
@@ -8,11 +8,8 @@ async function getVagas() {
 getVagas().then(vagas => vagas.forEach(vaga => {
     let linha = document.getElementById("linha");
 
-    let coluna = document.createElement("div");
-    //  adiciona a classe "col-4" ao elemento da coluna e "mb-3" para dar uma margem entre as linhas.
-    coluna.classList.add("col-4", "mb-5");
-    coluna.innerHTML = "<div class='card' style='width: 18rem;'><div class='card-body'><h5 class='card-title'>" + vaga.nomedavaga + "</h5>  <p class='card-text'> " + vaga.descricaodavaga + " </p> </div><ul class='list-group list-group-flush'><li class='list-group-item'>" + vaga.nomedacidade + "</li><li class='list-group-item'>" + vaga.nomedaempresa + "</li></ul><a href='#' class='btn btn-primary'>Candidatar se</a></div>";
+    linha.innerHTML += "<div class='col'><div class='card' style='width: 310px; height: 350px; background-color: #f3f3f3;'><div class='card-body'><h5 class='card-title'>" + vaga.nomedavaga + "</h5>  <p class='card-text'> "
+    + vaga.descricaodavaga + " </p> </div><ul class='list-group list-group-flush'><li  style= 'background-color: #f3f3f3;' class='list-group-item'>"
+    + vaga.nomedacidade + "</li><li style= 'background-color: #f3f3f3;' class='list-group-item'>" + vaga.nomedaempresa + "</li></ul><a href='#' class='btn btn-primary'>Candidatar se</a></div></div>";
 
-    linha.appendChild(coluna);
-    //adiciona uma coluna rescem cria a linha.
 }));
